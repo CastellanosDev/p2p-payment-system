7125585f2dcd4d32108ccd3d3a8ee7844eaae38a
Ignacio Castellanos <ignacio.castemore@gmail.com>
2026-02-03 10:37:02 +0100

    feat: initial Spring Boot project structure

---
+++ src/test/java/com/fintech/p2p/P2pPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.p2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2pPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
